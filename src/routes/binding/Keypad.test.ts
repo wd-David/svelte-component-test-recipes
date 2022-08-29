f44415d0b6481fef27dff8a7f714e1355824204a
davipon <davipondev@gmail.com>
2022-08-30 07:20:38 +0800

    feat: add two-way binding demo test

--- src/routes/binding/Keypad.test.ts
+++ src/routes/binding/Keypad.test.ts
@@ -1,9 +1,12 @@
-import {writable} from 'svelte/store'
+import {get, writable} from 'svelte/store'
 import { render, screen } from '@testing-library/svelte'
 import html from 'svelte-htm'
 import Keypad from './Keypad.svelte'
+import userEvent from '@testing-library/user-event'
+
 
 describe('Test Keypad component', async () => {
+  const user = userEvent.setup()
   it('Test two-way binding', async () => {
     const pin = writable('')
     const mock = vi.fn()
@@ -11,9 +14,22 @@ describe('Test Keypad component', async () => {
      * FIXME:
      * https://github.com/kenoxa/svelte-htm#readme
      * https://github.com/sveltejs/svelte/issues/6584
+     * https://github.com/sveltejs/svelte/pull/6646
+     * https://github.com/svelte-society/recipes-mvp/blob/2c7587ad559b3ee22a0caf5e1528bbac34dd475d/testing.md
      */
     render(html`<${Keypad} bind:value=${pin} on:submit=${mock}/>`)
+    const button1 = screen.getByText('1')
+    await user.click(button1)
+    await user.click(button1)
+    expect(get(pin)).toBe('11')
 
+    const submitButton = screen.getByText('submit')
+    await user.click(submitButton)
+    expect(mock).toHaveBeenCalled()
+
+    const clearButton = screen.getByText('clear')
+    await user.click(clearButton)
+    expect(get(pin)).toBe('')
   })
 })
 
--- src/routes/events/ComponentEvent.test.ts
+++ src/routes/events/ComponentEvent.test.ts
@@ -1,15 +1,17 @@
-import { render, screen, fireEvent } from '@testing-library/svelte';
+import { render, screen } from '@testing-library/svelte';
 import ComponentEvent from './ComponentEvent.svelte'
+import userEvent from '@testing-library/user-event'
 
 it('Test ComponentEvent component', async () => {
+  const user = userEvent.setup()
   const { component } = render(ComponentEvent)
   // Mock function
   let text = ''
   const mock = vi.fn((event) => text = event.detail.text)
   component.$on('message', mock)
-  
+
   const button = screen.getByRole('button')
-  await fireEvent.click(button)
+  await user.click(button)
 
   expect(mock).toHaveBeenCalled()
   expect(text).toBe('Hello!')
